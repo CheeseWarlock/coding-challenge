91b2aad346816b76de515ba03853be56277d07c8
Joe Cardwell <joe.cardwell@gmail.com>
2017-05-27 22:11:36 +0000

    Move logic into Repo component and display into RepoList component

---
+++ src/Repo.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class Repo extends Component {
+  render() {
+    const repo = this.props.repo;
+    return (
+      <div className="repo">
+        <h2>
+          <i className="fa fa-star" />
+          <a href={ repo.html_url }>
+            { repo.full_name }
+          </a>
+        </h2>
+        <p>
+          { repo.description }
+        </p>
+        <div className="counters">
+          <div className="stars">
+            <i className="fa fa-star" />
+            <a href={ repo.stargazers_url }>
+              { repo.stargazers_count }
+            </a>
+          </div>
+          <div className="forks">
+            <i className="fa fa-code-fork" />
+            <a href={ repo.forks_url }>
+              { repo.forks_count }
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Repo;
---
+++ src/RepoList.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Repo from './Repo.js';
+
+class RepoList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      repos: []
+    };
+    this.loadData();
+  }
+
+  loadData() {
+    const url = `https://api.github.com/users/${ this.props.user }/starred`;
+
+    fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          repos: data
+        });
+      });
+  }
+
+  render() {
+    const repoElements = this.state.repos.map((repo) => {
+      return (
+        <Repo repo={ repo }/>
+      );
+    });
+    return (
+      <div className="RepoList" id="container">
+        <h1>
+          { "Starred repos for " + this.props.user }
+        </h1>
+        { repoElements }
+      </div>
+    );
+  }
+}
+
+export default RepoList;
